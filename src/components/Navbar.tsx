cbfbaf2ee26f9d0b3d6f5af2c6ff1bed309725ca
KHarrisDev <kharrisjr.dev@gmail.com>
2024-07-16 19:31:39 -0400

    Creating Navbar
    Adding responsive widths and height to the Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,11 @@
+import Link from "next/link";
+
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div className="h-20 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
+      Navbar
+      {/* Mobile */}
+      <Link href="/">SUMTHINGSINABOX</Link>
+    </div>
+  );
 }
