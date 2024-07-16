b74b628c6953259de448a274fa045eedfbf92b38
KHarrisDev <kharrisjr.dev@gmail.com>
2024-07-16 19:52:38 -0400

    Mobile Menu Component
    Created a client component to display onClick actions for the menu icon and main menu in the menu component. Set the state for the Menu Component to either display the main menu or the hamburger icon when true or false. Imported the menu component to the Navbar.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,15 @@
 import Link from "next/link";
 
+import Menu from "./Menu";
+
 export default function Navbar() {
   return (
     <div className="h-20 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
       Navbar
       {/* Mobile */}
-      <Link href="/">SUMTHINGSINABOX</Link>
+      <Link href="/">
+        <Menu />
+      </Link>
     </div>
   );
 }
