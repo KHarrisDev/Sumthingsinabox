27a656db3b743469a7c0beee284b3780df949d99
KHarrisDev <kharrisjr.dev@gmail.com>
2024-07-24 15:26:23 -0400

    Creating links for Navbar
    Increased the width navbar for xl media query to display links in the Navbar.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,21 +11,28 @@ export default function Navbar() {
       {/* Mobile */}
       <div className="h-full flex items-center justify-between md:hidden">
         <Link href="/">
-          <div className="text-2xl tracking-wide">SUMTHINGSINABOX</div>
+          <div className="text-2xl tracking-wide">SIAB</div>
         </Link>
         <Menu />
       </div>
       {/* Bigger Screen Sizes */}
       <div className="hidden md:flex items-center justify-between gap-8 h-full">
         {/* Left */}
-        <div className="w-1/3">
+        <div className="w-1/3 xl:w-1/2 flex items-center gap-12">
           <Link href="/" className="flex items-center gap-3">
             <Image src="/logo.png" alt="" width={24} height={24} />
-            <div className="text-2xl tracking-wide">SUMTHINGSINABOX</div>
+            <div className="text-2xl tracking-wide">SIAB</div>
           </Link>
+          <div className="hidden xl:flex gap-4">
+            <Link href="/">Homepage</Link>
+            <Link href="/">Shop</Link>
+            <Link href="/">Deals</Link>
+            <Link href="/">About</Link>
+            <Link href="/">Contact</Link>
+          </div>
         </div>
         {/* Right */}
-        <div className="w-2/3 flex items-center justify-between gap-8">
+        <div className="w-2/3 xl:w-1/2 flex items-center justify-between gap-8">
           <SearchBar />
           <NavIcons />
         </div>
